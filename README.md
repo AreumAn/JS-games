b8428077b861dc9bf1e2984054df9bc32e03b5af
AREUM AN <areum.an.0928@gmail.com>
2020-05-23 03:26:19 -0400

    Add tic-tac-toe game

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ Have fun with simple games with JS for fun
 - [JS-games Intro](#js-games-intro)
 - [Table of Contents](#table-of-contents)
 - [Bulls and Cows](#bulls-and-cows)
+- [Tic Tac Toe](#tic-tac-toe)
 
 ---
 
@@ -15,3 +16,9 @@ Have fun with simple games with JS for fun
 Bulls and Cows (also known as Cows and Bulls or Pigs and Bulls) is an old code-breaking mind or paper and pencil game for two or more players, predating the commercially marketed board game Mastermind.
 
 To check more information[wikipedia bulls and cows](https://en.wikipedia.org/wiki/Bulls_and_Cows)
+
+## Tic Tac Toe
+
+Tic-tac-toe (American English), noughts and crosses (British English), or Xs and Os is a paper-and-pencil game for two players, X and O, who take turns marking the spaces in a 3×3 grid. The player who succeeds in placing three of their marks in a horizontal, vertical, or diagonal row is the winner.
+
+To check more information[wikipedia Tic Tac Toe](https://en.wikipedia.org/wiki/Tic-tac-toe)
---
+++ tic-tac-toe.js
@@ -0,0 +1,87 @@
+const body = document.body;
+
+let table = document.createElement("table");
+let tableArr = [];
+// player: X or O
+let turn = "X";
+
+// winning case
+const winningArr = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+// Change player
+function changeTurn() {
+  turn === "X" ? (turn = "O") : (turn = "X");
+}
+
+// Get element text by id
+function getTextContent(id) {
+  return document.getElementById(id).textContent;
+}
+
+// Check if player wins
+function checkWinner(clickedID) {
+  for (let i = 0; i < winningArr.length; i++) {
+    if (winningArr[i].indexOf(clickedID) !== -1) {
+      const a = getTextContent(winningArr[i][0]);
+      const b = getTextContent(winningArr[i][1]);
+      const c = getTextContent(winningArr[i][2]);
+
+      return turn === a && turn === b && turn === c;
+    }
+  }
+}
+
+// When player clicks grid.
+const clickEvent = function (event) {
+  const clickedBox = event.target;
+
+  if (clickedBox.textContent !== "") {
+    // If other grid is already checked
+    alert("You can not click this.");
+    return;
+  } else if (turn === "X") {
+    clickedBox.textContent = "X";
+  } else {
+    clickedBox.textContent = "O";
+  }
+
+  // Check if player wins
+  if (checkWinner(parseInt(clickedBox.id))) {
+    const title = document.getElementById("title");
+    let winnerText = document.createElement("h2");
+    winnerText.style.color = "red";
+    winnerText.textContent =
+      turn + " WIN!! This page will be reload in 10 sec. ";
+    title.after(winnerText);
+    setTimeout(function () {
+      location.reload();
+    }, 10000);
+  } else {
+    // Change turn to other player
+    changeTurn();
+  }
+};
+
+// Draw grid with id
+for (let i = 0; i < 3; i++) {
+  let row = document.createElement("tr");
+  for (let j = 0; j < 3; j++) {
+    let col = document.createElement("td");
+    col.id = tableArr.length;
+    col.addEventListener("click", clickEvent);
+    tableArr.push(tableArr.length);
+    row.appendChild(col);
+  }
+  table.appendChild(row);
+}
+
+body.appendChild(table);
