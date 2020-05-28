61f304a1772f07aab5ab3be6c5ff1e20caabffbb
AREUM AN <areum.an.0928@gmail.com>
2020-05-28 18:00:43 -0400

    Add right,left click at the same time

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ Have fun with simple games with JS for fun
 - [Tic Tac Toe](#tic-tac-toe)
 - [Lotto](#lotto)
 - [Rock Paper Scissors](#rock-paper-scissors)
-- [Minesweeper](#minesweeper)
+- [Minesweeper(WIP)](#minesweeperwip)
 
 ---
 
@@ -32,6 +32,8 @@ Get random lotto number. It shows 6 number and 1 bonus number. A number ball app
 
 ## Rock Paper Scissors
 
-## Minesweeper
+## Minesweeper(WIP)
 
 Minesweeper is a single-player puzzle video game. The objective of the game is to clear a rectangular board containing hidden "mines" or bombs without detonating any of them, with help from clues about the number of neighboring mines in each field. The game originates from the 1960s, and has been written for many computing platforms in use today. It has many variations and offshoots. To check more information [wikipedia Minesweeper](<https://en.wikipedia.org/wiki/Minesweeper_(video_game)>)
+
+**TODO: timer, error, refactor**
--- minesweeper.js
+++ minesweeper.js
@@ -1,12 +1,22 @@
 let tbody = document.querySelector("#table tbody");
+let resultDiv = document.querySelector("#result");
+/**
+ * dataSet
+ *
+ * 0: default - not opened, no bomb
+ * 1: opned
+ * X: bomb
+ */
 let dataSet = [];
+let isStopGame = false;
+let openedGrid = 0;
 
 // Get bombs random indx
 function getBombs(row, column, bomb) {
   // get all possible idx
   let allIdx = Array(row * column)
     .fill()
-    .map(function (el, idx) {
+    .map((el, idx) => {
       return idx;
     });
 
@@ -23,11 +33,14 @@ function getBombs(row, column, bomb) {
 
 function resetGame() {
   tbody.innerHTML = "";
+  resultDiv.textContent = "";
   dataSet = [];
+  isStopGame = false;
+  openedGrid = 0;
 }
 
-// Get how many bombs are around
-function getBombsNum(row, col) {
+// Get dataSet values around dataSet[row][col]
+function getAroundDataValue(row, col) {
   let aroundArr = [dataSet[row][col - 1], dataSet[row][col + 1]];
   if (dataSet[row - 1]) {
     aroundArr = aroundArr.concat(
@@ -43,9 +56,62 @@ function getBombsNum(row, col) {
       dataSet[row + 1][col + 1]
     );
   }
-  return aroundArr.filter((v) => {
+  return aroundArr;
+}
+
+//Get td elements around tbody.children[row].children[col] which the user clicked
+function getAroundGrid(row, col) {
+  let aroundTDArr = [
+    tbody.children[row].children[col - 1],
+    tbody.children[row].children[col + 1],
+  ];
+  if (tbody.children[row - 1]) {
+    aroundTDArr = aroundTDArr.concat(
+      tbody.children[row - 1].children[col - 1],
+      tbody.children[row - 1].children[col],
+      tbody.children[row - 1].children[col + 1]
+    );
+  }
+  if (tbody.children[row + 1]) {
+    aroundTDArr = aroundTDArr.concat(
+      tbody.children[row + 1].children[col - 1],
+      tbody.children[row + 1].children[col],
+      tbody.children[row + 1].children[col + 1]
+    );
+  }
+
+  return aroundTDArr;
+}
+
+// Get how many bombs are around
+function getBombsNum(row, col) {
+  return getAroundDataValue(row, col).filter((v) => {
     return v === "X";
   }).length;
+}
+
+// Open girds around when it doesn't have bombs
+function openAroundGrid(row, col) {
+  getAroundGrid(row, col)
+    .filter((v) => !!v) // filter undefined
+    .forEach(function (td) {
+      td.click();
+    });
+}
+
+// Draw flag or question on the grid
+function drawFlag(target) {
+  if (target.textContent === "!") {
+    target.textContent = "?";
+    target.classList.remove("flag");
+    target.classList.add("question");
+  } else if (target.textContent === "?") {
+    target.classList.remove("question");
+    target.textContent = "";
+  } else {
+    target.classList.add("flag");
+    target.textContent = "!";
+  }
 }
 
 document.querySelector("#exec").addEventListener("click", (e) => {
@@ -54,8 +120,11 @@ document.querySelector("#exec").addEventListener("click", (e) => {
   const row = document.querySelector("#hor").value;
   const column = document.querySelector("#ver").value;
   const bomb = document.querySelector("#bomb").value;
-
-  console.log(row, column, bomb);
+  let bombsIdx = getBombs(row, column, bomb);
+  let pressKeys = {
+    right: false,
+    left: false,
+  };
 
   // Draw table
   for (let i = 0; i < row; i++) {
@@ -63,30 +132,108 @@ document.querySelector("#exec").addEventListener("click", (e) => {
     let tr = document.createElement("tr");
     dataSet.push(arr);
     for (let j = 0; j < column; j++) {
-      arr.push(1);
+      arr.push(0);
       let td = document.createElement("td");
+
+      td.addEventListener("mousedown", (e) => {
+        if (e.which == 1) {
+          pressKeys.left = true;
+        } else if (e.which === 3) {
+          pressKeys.right = true;
+        }
+
+        // Click right, left button at the same time
+        if (pressKeys.right && pressKeys.left) {
+          pressKeys.right = false;
+          console.log(i, j, dataSet[i][j]);
+          if (dataSet[i][j] === 1) {
+            // check if bomb has X but it has not flag or question, change color
+            let array = getAroundGrid(i, j);
+            let values = getAroundDataValue(i, j);
+            let matched = [];
+            let temp = [];
+            console.log("array: ", array);
+            console.log("values: ", values);
+
+            for (let i = 0; i < array.length; i++) {
+              if (
+                values[i] === "X" &&
+                !!array[i].textContent &&
+                array[i].textContent !== "X"
+              ) {
+                matched.push(array[i]);
+              } else if (values[i] !== 1) {
+                temp.push(array[i]);
+              }
+            }
+
+            if (matched.length === getBombsNum(i, j)) {
+              temp.forEach((td) => {
+                td.click();
+              });
+            } else {
+              temp.forEach((td) => {
+                td.classList.add("hoverAnimation");
+                console.log(td.classList);
+              });
+            }
+          }
+        }
+      });
+      td.addEventListener("mouseup", (e) => {
+        if (e.which == 1) {
+          pressKeys.left = false;
+        } else if (e.which === 3) {
+          pressKeys.right = false;
+        }
+        let hoveredEls = document.querySelectorAll(".hoverAnimation");
+        for (let i = 0; i < hoveredEls.length; i++) {
+          hoveredEls[i].classList.remove("hoverAnimation");
+        }
+      });
       // Right click event for adding flag
       td.addEventListener("contextmenu", (e) => {
         e.preventDefault();
-        if (e.target.textContent === "!") {
-          e.target.textContent = "?";
-          dataSet[i][j] = "?";
-        } else if (e.target.textContent === "?") {
-          e.target.textContent = "";
-          dataSet[i][j] = dataSet[i][j] === "X" ? "X" : 1;
-        } else {
-          e.target.textContent = "!";
-          dataSet[i][j] = "!";
+        if (isStopGame || dataSet[i][j] === 1) {
+          // return if game is done or this grid is already opened
+          return;
         }
+        if (!pressKeys.left) {
+          drawFlag(e.target);
+        }
+        pressKeys.right = false;
       });
       // left click to get number of bombs
       td.addEventListener("click", (e) => {
+        console.log("hello");
+        if (isStopGame) {
+          return;
+        }
+        // When the user clicks right and left button at the same time
+        console.log("click: ", pressKeys.right, pressKeys.left);
+
         if (dataSet[i][j] === "X") {
           // If user clicks bomb, user will lose.
-          console.log("You lose!");
-        } else {
+          isStopGame = true;
+          resultDiv.textContent = "You Lose!";
+        } else if (dataSet[i][j] !== 1) {
           // Show bombs number around clicked grid
-          e.target.textContent = getBombsNum(i, j);
+          let bombsAroundTarget = getBombsNum(i, j);
+          dataSet[i][j] = 1;
+          openedGrid += 1;
+          // If bombsAroundTarget is falsy - false, '', 0, null, undefined, NaN -
+          // Use "" instead
+          e.target.textContent = bombsAroundTarget || "";
+          e.target.classList.add("opened");
+          // If target doesn't have bomb around, open around grid
+          if (bombsAroundTarget === 0) {
+            openAroundGrid(i, j);
+          }
+        }
+
+        if (openedGrid >= row * column - bomb) {
+          isStopGame = true;
+          resultDiv.textContent = "You win!";
         }
       });
       tr.appendChild(td);
@@ -94,16 +241,11 @@ document.querySelector("#exec").addEventListener("click", (e) => {
     tbody.appendChild(tr);
   }
 
-  let bombsIdx = getBombs(row, column, bomb);
-  console.log("bombsIdx :", bombsIdx);
-
   // Add bombs
   for (let i = 0; i < bombsIdx.length; i++) {
-    let rowPosition = Math.floor(bombsIdx[i] / 10);
-    let colPosition = bombsIdx[i] % 10;
+    let rowPosition = Math.floor(bombsIdx[i] / column);
+    let colPosition = bombsIdx[i] % column;
     tbody.children[rowPosition].children[colPosition].textContent = "X";
     dataSet[rowPosition][colPosition] = "X";
   }
-
-  console.log(dataSet);
 });
